d995fc206840ab8445819e85c5e79a0299a13857
NadulaG <nadula.gardiyehewa@gmail.com>
2024-03-02 14:03:46 -0500

    Started main page on website

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,4 +1,30 @@
-<h1 class="text-white">Hello world!</h1>
+<!-- TOP BAR -->
+<div class="bg-[#1B1B1B] py-2">
+  <div class="container ml-8 mx-auto">
+    <div class="flex justify-between">
+      <div class="text-white text-2xl font-bold">Flight History</div>
+      <div class="flex items-center space-x-4">
+        <span class="text-gray-300">3/2/2023</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="flex h-max">
+  <!-- SIDE BAR -->
+  <div class="bg-[#1B1B1B] w-64">
+    <div class="flex flex-col items-center h-full">
+      <div class="text-white text-2xl font-bold">Summary</div>
+      <div class="flex flex-col items-center space-y-4 mt-4">
+        <!-- <a href="/+page" class="text-white">Home</a> -->
+      </div>
+    </div>
+  </div>
+
+  <div class="flex justify-center flex-1">
+    <img src="/landscape.png" alt="landscape" class="w-full h-fit" />
+  </div>
+</div>
 
 <style lang="postcss">
   :global(html) {
